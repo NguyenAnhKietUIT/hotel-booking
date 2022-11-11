f49431cb4c79e0135d53bfeb8d1aba5fc2f0c3bd
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2022-11-11 14:48:50 +0700

    new change

--- home_page/search_history.js
+++ home_page/search_history.js
@@ -1,6 +1,18 @@
-const itemHistory = document.querySelector('.app__search-history-item');
+const inputSearch = document.querySelector('input[name="app__search-input"]')
+const boxHistory = document.querySelector('.app__search-history')
+const listHistory = document.querySelector('.app__search-history-list')
 
-itemHistory.addEventListener('click', () => {
-    alert()
-    console.log(itemHistory)
+inputSearch.addEventListener('click', () => {
+    boxHistory.style.display = 'block'
+})
+
+inputSearch.addEventListener('input', () => {
+    //events find similar
+})
+
+listHistory.addEventListener('click', (events) => {
+    let result = events.target.innerText
+
+    inputSearch.value = result
+    boxHistory.style.display = 'none'
 })
