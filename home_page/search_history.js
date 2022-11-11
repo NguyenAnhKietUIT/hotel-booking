d598fd9c42d8b9c3abd2a691d73db220beafcd7e
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2022-11-11 19:56:30 +0700

    responsive home page

--- home_page/search_history.js
+++ home_page/search_history.js
@@ -1,18 +1,31 @@
 const inputSearch = document.querySelector('input[name="app__search-input"]')
 const boxHistory = document.querySelector('.app__search-history')
 const listHistory = document.querySelector('.app__search-history-list')
+const itemHistory = document.querySelectorAll('.app__search-history-item')
 
 inputSearch.addEventListener('click', () => {
     boxHistory.style.display = 'block'
 })
 
 inputSearch.addEventListener('input', () => {
-    //events find similar
+    let currentInput = inputSearch.value
+
+    itemHistory.forEach(element => {
+        if (!element.innerText.includes(currentInput)) {
+            element.style.display = 'none'
+        } else {
+            element.style.display = 'block'
+        }
+    })
 })
 
 listHistory.addEventListener('click', (events) => {
     let result = events.target.innerText
 
     inputSearch.value = result
+    boxHistory.style.display = 'none'
+})
+
+boxHistory.addEventListener('mouseleave', () => {
     boxHistory.style.display = 'none'
 })
