f169e56caa0174cf9a2af6acb1e02283b8a61dfb
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2022-12-01 00:21:15 +0700

    add some event admin page

--- js/Admin.js
+++ js/Admin.js
@@ -81,4 +81,59 @@ function changeBackground() {
     }
 }
 
+function handleHeader() {
+    $(function() {
+        "use strict";
+        
+        $(".toggle-menu").on("click", function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });	 	   
+
+        $(document).ready(function(){ 
+            $(window).on("scroll", function(){ 
+                if ($(this).scrollTop() > 60) { 
+                    $('.topbar-nav .navbar').addClass('bg-dark'); 
+                } else { 
+                    $('.topbar-nav .navbar').removeClass('bg-dark'); 
+                } 
+            });
+        
+        });
+     
+        $(document).ready(function(){ 
+            $(window).on("scroll", function(){ 
+                if ($(this).scrollTop() > 300) { 
+                    $('.back-to-top').fadeIn(); 
+                } else { 
+                    $('.back-to-top').fadeOut(); 
+                } 
+            }); 
+        
+            $('.back-to-top').on("click", function(){ 
+                $("html, body").animate({ scrollTop: 0 }, 600); 
+                return false; 
+            }); 
+        });	   
+
+        $(function () {
+          $('[data-toggle="popover"]').popover()
+        })
+
+        $(function () {
+          $('[data-toggle="tooltip"]').tooltip()
+        })
+
+        $('.right-nav-link .nav-item:last-child').on('click', function() {
+            $('.dropdown-menu').toggleClass('show');
+        });
+
+        $('.dropdown-menu').on('click', function(e) {
+            e.stopPropagation();
+        })
+    })
+}
+
 changeBackground()
+
+handleHeader()
