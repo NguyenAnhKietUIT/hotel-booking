f89f3998ab4a66c7f6b1c6ef4627424602733dcf
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2022-12-05 02:43:00 +0700

    update admin_message

--- js/Message.js
+++ js/Message.js
@@ -3,6 +3,10 @@ const checkboxMessage = document.querySelectorAll('.message-select')
 const messageTrash = document.querySelectorAll('.message-trash')
 const starNone = document.querySelectorAll('.message-icon')
 const starActive = document.querySelectorAll('.message-icon-active')
+const boxMessage = document.querySelectorAll('.table')
+const buttonDelete = document.querySelector('.nav-message-delete')
+const boxListMessage = document.querySelector('.box-list-message')
+const listMessage = document.querySelectorAll('.table tr')
 
 function selectAll() {
     checkboxSelectAll.addEventListener('change', (event) => {
@@ -56,7 +60,144 @@ function selectMessage() {
     })
 }
 
+function deleteMessageChecked() {
+    buttonDelete.addEventListener('click', () => {
+        checkboxMessage.forEach(element => {
+            if (element.checked) {
+                element.parentElement.parentElement.remove()
+            }
+        })
+    })
+}
+
+function handleReplyBox() {
+    const buttonReply = document.querySelector('.btn-reply')
+    const replyBox = document.querySelector('.message-home__form')
+    const deleteBoxReply = document.querySelector('.message-home__delete')
+
+    buttonReply.addEventListener('click', () => {
+        replyBox.style.display = 'block'
+    })
+
+    deleteBoxReply.addEventListener('click', () => {
+        replyBox.style.display = 'none'
+    })
+}
+
+function watchMessage() {
+    listMessage.forEach(element => {
+        element.getElementsByTagName("td")[2].addEventListener('click', () => {
+            boxListMessage.style.display = 'none'
+
+            let container = document.querySelector('.content-wrapper')
+            let html = ""
+
+            html += `
+                    <div class="message-home">
+                        <nav class="message-home__nav m-3">
+                            <span>
+                                <i class="message-home__back fa-solid fa-arrow-left"></i>
+                            </span>
+
+                            <span>${element.querySelector('.message-title').textContent}</span>
+                        </nav>
+
+                        <div class="message-home__main m-3">
+                            <span>${element.getElementsByTagName("td")[2].textContent}</span>
+                            <p>${element.querySelector('.message-content').textContent}</p>
+                        </div>
+                            
+                        <div class="message-home__reply">
+                            <button class="btn-reply ms-3">
+                                <i class="fa-solid fa-reply"></i>
+                                Reply
+                            </button>
+                                    
+                            <div class="message-home__form">
+                                <form action="" method="" id="message-form">
+                                    <p>${element.getElementsByTagName("td")[2].textContent}</p>
+                                    <input type="text" name="message-home__subject" id="message-home__subject" placeholder="Subject">
+                                    <textarea name="message-home__content" id="" cols="30" rows="6" placeholder="Your message"></textarea>
+                                    <div class="message-home__action">
+                                        <input type="submit" value="Send" id="message-home__submit">
+                                        <span class="message-home__delete">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    `
+            container.innerHTML = html
+
+            handleReplyBox()
+            backAdminMessage()
+        })
+        element.getElementsByTagName("td")[3].addEventListener('click', () => {
+            boxListMessage.style.display = 'none'
+
+            let container = document.querySelector('.content-wrapper')
+            let html = ""
+
+            html += `
+                    <div class="message-home">
+                        <nav class="message-home__nav m-3">
+                            <span>
+                                <i class="message-home__back fa-solid fa-arrow-left"></i>
+                            </span>
+
+                            <span>${element.querySelector('.message-title').textContent}</span>
+                        </nav>
+
+                        <div class="message-home__main m-3">
+                            <span>${element.getElementsByTagName("td")[2].textContent}</span>
+                            <p>${element.querySelector('.message-content').textContent}</p>
+                        </div>
+                            
+                        <div class="message-home__reply">
+                            <button class="btn-reply ms-3">
+                                <i class="fa-solid fa-reply"></i>
+                                Reply
+                            </button>
+                                    
+                            <div class="message-home__form">
+                                <form action="" method="" id="message-form">
+                                    <p>${element.getElementsByTagName("td")[2].textContent}</p>
+                                    <input type="text" name="message-home__subject" id="message-home__subject" placeholder="Subject">
+                                    <textarea name="message-home__content" id="" cols="30" rows="6" placeholder="Your message"></textarea>
+                                    <div class="message-home__action">
+                                        <input type="submit" value="Send" id="message-home__submit">
+                                        <span class="message-home__delete">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    `
+            container.innerHTML = html
+
+            handleReplyBox()
+            backAdminMessage()
+        })
+    })
+}
+
+function backAdminMessage() {
+    const backHome = document.querySelector('.message-home__back')
+    const messageHome = document.querySelector('.message-home')
+
+    backHome.addEventListener('click', () => {
+        //render message
+        location.reload()
+    })
+}
+
 selectAll()
 deleteMessage()
 starMessage()
 selectMessage()
+deleteMessageChecked()
+watchMessage()
