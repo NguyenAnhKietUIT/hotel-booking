7056fdbdc1c2321847671b7ef1be01c6eb4ca793
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2022-12-08 17:15:18 +0700

    update cus info js

--- js/Customer_Information.js
+++ js/Customer_Information.js
@@ -21,163 +21,137 @@ function mainFunction() {
     })
 
     function viewAllBookings() {
-        // let upcomings = [
-        //     {
-        //         img: "../assets/img/others/mother_home.png",
-        //         title: "Mother's Home",
-        //         desc: "6 Aug - 7 Aug • Ho Chi Minh City",
-        //         price: "$21",
-        //     },
-        //     {
-        //         img: "../assets/img/others/mother_home.png",
-        //         title: "Mother's Home",
-        //         desc: "6 Aug - 7 Aug • Ho Chi Minh City",
-        //         price: "$21",
-        //     },
-        // ]
-        
-        // let pasts = [
-        //     {
-        //         img: "../assets/img/others/mother_home.png",
-        //         title: "Mother's Home",
-        //         desc: "6 Aug - 7 Aug • Ho Chi Minh City",
-        //         status: "complete",
-        //         price: "$21",
-        //     },
-        //     {
-        //         img: "../assets/img/others/mother_home.png",
-        //         title: "Mother's Home",
-        //         desc: "6 Aug - 7 Aug • Ho Chi Minh City",
-        //         status: "canceled",
-        //         price: "$21",
-        //     },
-        // ]
+        let upcomingAPI = "";
+        let pastAPI = "";
+        let html = "";
+        let flag = false;
 
-        let upcomings
-        let pasts
+        fetch (upcomingAPI)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(upcomings){
+                if (upcomings.length > 0) {
+                    flag = true;
 
-        let html = ""
-
-        if (upcomings) {
-            html += `
+                    html += `
                     <h2 class="app__view-message">Your bookings</h2>
 
                     <div class="app__view-list">
                         <h3 class="app__view-title">Upcoming</h3>
-                    `
+                    `;
 
-            html += upcomings.map(function(upcoming){
-                return `
-                    <a href="#" class="app__view-item">
-                        <img src="${upcoming.img}" alt="Type Room" class="app__view-img">
-                        <div class="app__view-item-info">
-                            <h4 class="app__view-item-title">${upcoming.title}</h4>
-                            <span class="app__view-item-desc">${upcoming.desc}</span>
-                        </div>
-                        <h3 class="app__view-item-price">${upcoming.price}</h3>
-                    </a>
-                `
-            }).join('')
-            html += `</div>`
-        }
+                    html += upcomings.map(function(upcoming){
+                        return `
+                            <a href="#" class="app__view-item">
+                                <img src="${upcoming.img}" alt="Type Room" class="app__view-img">
+                                <div class="app__view-item-info">
+                                    <h4 class="app__view-item-title">${upcoming.title}</h4>
+                                    <span class="app__view-item-desc">${upcoming.desc}</span>
+                                </div>
+                                <h3 class="app__view-item-price">${upcoming.price}</h3>
+                            </a>
+                        `
+                    }).join('');
 
-        if (pasts) {
-            html += `
+                    html += `</div>`;
+                }
+            })
+            .catch(function (err) {
+                alert(err.message);
+            });
+
+        fetch (pastAPI)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(pasts){
+                if (pasts.length > 0) {
+                    flag = true;
+
+                    html += `
                     <div class="app__view-list app__view-list-status">
                         <h3 class="app__view-title">In the past</h3>
-                    `
-            html += pasts.map(function(past) {
-                return `
-                    <a href="#" class="app__view-item">
-                        <img src="${past.img}" alt="Type Room" class="app__view-img">
-                        <div class="app__view-item-info">
-                            <h4 class="app__view-item-title">${past.title}</h4>
-                            <span class="app__view-item-desc">${past.desc}</span>
-                            <span class="app__view-item-status ${(past.status === 'canceled') ? "app__view-item-status--active" : ""}">${past.status}</span>
-                        </div>
-                        <h3 class="app__view-item-price">${past.price}</h3>
-                    </a>
-                `
-            }).join('')
-            html += `</div>`
-        }
+                    `;
 
-        if (!upcomings && !pasts) {
+                    html += pasts.map(function(past) {
+                        return `
+                            <a href="#" class="app__view-item">
+                                <img src="${past.img}" alt="Type Room" class="app__view-img">
+                                <div class="app__view-item-info">
+                                    <h4 class="app__view-item-title">${past.title}</h4>
+                                    <span class="app__view-item-desc">${past.desc}</span>
+                                    <span class="app__view-item-status ${(past.status === 'canceled') ? "app__view-item-status--active" : ""}">${past.status}</span>
+                                </div>
+                                <h3 class="app__view-item-price">${past.price}</h3>
+                            </a>
+                        `
+                    }).join('');
+
+                    html += `</div>`;
+                }
+            })
+            .catch(function (err) {
+                alert(err.message);
+            });
+
+
+        if (!flag) {
             html += `
                     <span class="app__view-message">You have no bookings yet.</span>
                     <a href="../html/Homepage.html" class="app__view-link">Start exploring</a>
-                    `
+                    `;
         }
 
         mainContent.innerHTML = html
     }
 
     function viewFavorites() {
-        let favourites = [
-            {
-                img: "../assets/img/others/favourite.png",
-                title: "Hotel name",
-                desc: "Ho Chi Minh City, Vietnam",
-                view: "8.9/161 reviews",
-                price: "$21",
-            },
-            {
-                img: "../assets/img/others/favourite.png",
-                title: "Hotel name",
-                desc: "Ho Chi Minh City, Vietnam",
-                view: "8.9/161 reviews",
-                price: "$21",
-            },
-            {
-                img: "../assets/img/others/favourite.png",
-                title: "Hotel name",
-                desc: "Ho Chi Minh City, Vietnam",
-                view: "8.9/161 reviews",
-                price: "$21",
-            },
-            {
-                img: "../assets/img/others/favourite.png",
-                title: "Hotel name",
-                desc: "Ho Chi Minh City, Vietnam",
-                view: "8.9/161 reviews",
-                price: "$21",
-            },
-        ]
+        let favouriteAPI = "";
+        let html = "";
 
-        // let favourites
-        let html = ""
-
-        if (favourites) {
-            html += `
-                    <div class="app__view-list app__view-list-status">
-                        <h3 class="app__view-title">Your favourites</h3>
-                    `
-            html += favourites.map(function(favourite) {
-                return `
-                    <a href="#" class="app__view-item">
-                        <img src="${favourite.img}" alt="Type Room" class="app__view-img">
-                        <div class="app__view-item-info">
-                            <h4 class="app__view-item-title">${favourite.title}</h4>
-                            <span class="app__view-item-desc">${favourite.desc}</span>
-                            <span class="app__view-item-status">${favourite.view}</span>
+        fetch (favouriteAPI)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function(favourites){
+                if (favourites.length > 0) {
+                    html += `
+                            <div class="app__view-list app__view-list-status">
+                                <h3 class="app__view-title">Your favourites</h3>
+                            `;
+        
+                    html += favourites.map(function(favourite) {
+                        return `
+                            <a href="#" class="app__view-item">
+                                <img src="${favourite.img}" alt="Type Room" class="app__view-img">
+                                <div class="app__view-item-info">
+                                    <h4 class="app__view-item-title">${favourite.title}</h4>
+                                    <span class="app__view-item-desc">${favourite.desc}</span>
+                                    <span class="app__view-item-status">${favourite.view}</span>
+                                </div>
+                                <h3 class="app__view-item-price">${favourite.price}</h3>
+                            </a>
+                        `
+                    }).join('');
+        
+                    html += `</div>`;
+                } else {
+                    html += `
+                        <div class="app__favourite-none">
+                            <i class="app__favourite-none-icon fa-regular fa-heart"></i>
+                            <span class="app__favourite-none-title">Save your favourites</span>
+                            <p class="app__favourite-none-desc">
+                                When you find something you like, use the heart to </br> 
+                                save your favourites and you can find it again here.
+                            </p>
                         </div>
-                        <h3 class="app__view-item-price">${favourite.price}</h3>
-                    </a>
-                `
-            }).join('')
-            html += `</div>`
-        } else {
-            html += `
-                <div class="app__favourite-none">
-                    <i class="app__favourite-none-icon fa-regular fa-heart"></i>
-                    <span class="app__favourite-none-title">Save your favourites</span>
-                    <p class="app__favourite-none-desc">
-                        When you find something you like, use the heart to </br> 
-                        save your favourites and you can find it again here.
-                    </p>
-                </div>
-            `
-        }
+                    `
+                }
+            })
+            .catch(function (err) {
+                alert(err.message);
+            });
 
         mainContent.innerHTML = html
     }
