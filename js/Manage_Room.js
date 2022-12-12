9c6ba2d31660707e9763dfd1cc2d930db3f52894
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2022-12-12 12:53:25 +0700

    update manage room

--- js/Manage_Room.js
+++ js/Manage_Room.js
@@ -1,7 +1,27 @@
-const buttonUpdate = document.querySelector('.btn.btn-info');
+const roomId = document.getElementById('room-id');
+const roomName = document.getElementById('room-name');
+const typeRoom = document.getElementById('type-room');
+const bedNum = document.getElementById('bed-num');
+const price = document.getElementById('price');
+const imageRoom = document.getElementById('img-room');
+const buttonUpdate = document.querySelectorAll('.btn.btn-info');
 
-buttonUpdate.addEventListener('click', () => {
-    
+buttonUpdate.forEach(element => {
+    element.addEventListener('click', () => {
+        let array = element.parentElement.parentElement.getElementsByTagName("td");
+
+        roomId.value = array[0].textContent;
+        roomName.value = array[1].textContent;
+        typeRoom.value = array[2].textContent;
+        bedNum.value = array[3].textContent;
+        price.value = parseInt(array[4].textContent);
+        document.getElementById('image').removeAttribute("required")
+        imageRoom.src = array[5].getElementsByTagName("img")[0].currentSrc;
+        imageRoom.removeAttribute("hidden");
+        imageRoom.style.display = "block";
+        imageRoom.style.width = "300px";
+        imageRoom.style.height = "200px";
+    })
 });
 
 function readURL(input) {
@@ -9,9 +29,9 @@ function readURL(input) {
         var reader = new FileReader();
   
         reader.onload = function (e) {
-            $('#img-property').removeAttr("hidden")
-            $('#img-property').attr("style", "display: block");
-            $('#img-property').attr('src', e.target.result).width(400).height(300);
+            $('#img-room').removeAttr("hidden")
+            $('#img-room').attr("style", "display: block");
+            $('#img-room').attr('src', e.target.result).width(300).height(200);
         };
 
         reader.readAsDataURL(input.files[0]);
